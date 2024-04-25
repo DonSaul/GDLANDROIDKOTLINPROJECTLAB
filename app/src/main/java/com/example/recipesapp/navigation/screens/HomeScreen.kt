292dc38b96d8a8a3cf92628be7a5c01674c8dc1c
CsarChvz <79390377+CsarChvz@users.noreply.github.com>
2024-04-24 21:29:48 -0600

    Screen squeleton with sections

--- app/src/main/java/com/example/recipesapp/navigation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/recipesapp/navigation/screens/HomeScreen.kt
@@ -1,2 +1,229 @@
-@Composable 
-fun HomeScreen(){}
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun HomeScreen() {
+    Scaffold(
+        modifier = Modifier.padding(12.dp, 24.dp, 0.dp, 0.dp)
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            SearchBar(
+                placeholder = "Search recipes...",
+            )
+            Text(
+                text = "Recomendaciones", modifier = Modifier.padding(start = 16.dp, top = 16.dp)
+            )
+            RecommendationsComponent()
+            Text(
+                text = "Recetas", modifier = Modifier.padding(start = 16.dp, top = 16.dp)
+            )
+            ReceiptsComponent()
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun SearchBar(
+    placeholder: String,
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    var searchText by remember { mutableStateOf("") }
+
+    Row(modifier = Modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            placeholder = { Text(placeholder) },
+            singleLine = true,
+            trailingIcon = {
+                IconButton(onClick = {
+                    keyboardController?.hide()
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search icon"
+                    )
+                }
+            },
+            keyboardActions = KeyboardActions(onSearch = {
+                keyboardController?.hide()
+            }),
+            modifier = Modifier.weight(1f)
+        )
+
+        IconButton(
+            onClick = { },
+            modifier = Modifier.weight(0.2f)
+        ) {
+            Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More options")
+        }
+    }
+}
+
+@Composable
+fun RecommendationsComponent() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(220.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        LazyRow {
+            items(items.size) {
+                RecommendedReceipt()
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ReceiptsComponent() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        LazyColumn {
+            items(receiptItems.size) {
+                RecipeCardComponent()
+            }
+        }
+    }
+}
+
+@Composable
+fun RecommendedReceipt() {
+    Column {
+        Card(
+            modifier = Modifier
+                .size(
+                    width = 160.dp,
+                    height = 160.dp
+                )
+                .padding(10.dp),
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+            }
+        }
+
+        Column(
+            modifier = Modifier
+                .padding(16.dp, 0.dp),
+        ) {
+            Text(text = "Receipt Name")
+            Text(text = "Description")
+
+        }
+    }
+}
+
+
+@Composable
+fun RecipeCardComponent(
+    recipeName: String = "Recipe Name",
+    description: String = "Description",
+    category: String = "Category",
+    onCategoryClicked: () -> Unit = {}
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(8.dp),
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(80.dp)
+                    .background(Color.Cyan), // Usa un placeholder en lugar de una imagen real
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = recipeName,
+                    style = MaterialTheme.typography.titleSmall,
+                    color = Color.Black
+                )
+                Text(
+                    text = description,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = Color.Gray
+                )
+            }
+        }
+    }
+}
+
+val items = listOf(
+    "",
+    "",
+    "",
+    "",
+    "",
+)
+
+
+val receiptItems = listOf(
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+)
+    
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HomeScreen()
+}
